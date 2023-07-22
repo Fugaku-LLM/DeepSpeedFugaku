5f32279cbb74db413d5df1a0fc631e069011557b
kazuki <kaz.tokyo.tech20@gmail.com>
2023-07-22 22:51:52 +0900

    feat: 13B ja tokenizer job script

---
+++ experiments/scripts/13b/JapaneseTokenizer/13b_mp20_dp16-ja40K-en10K.sh
@@ -0,0 +1,89 @@
+#!/bin/bash -x
+#PJM -L "rscunit=rscunit_ft01,rscgrp=small"
+#PJM -L elapse=72:00:00
+#PJM -L "node=320"
+#PJM --mpi "proc=320"
+#PJM --mpi "max-proc-per-node=1"
+#PJM -g hp190122
+#PJM -x PJM_LLIO_GFSCACHE=/vol0003:/vol0004
+#PJM -j
+#PJM -S
+
+source /data/hp190122/share/PyTorch-1.10.1/env.src
+export PYTHONUSERBASE=$HOME/work/.local
+export PATH=$PATH:$PYTHONUSERBASE/bin
+
+user_name=$(whoami)
+cd /home/$user_name/work/DeepSpeedFugaku
+
+JA_VOCAB_SIZE=40
+EN_VOCAB_SIZE=10
+
+# Change for multinode config
+CPUS_PER_NODE=1
+NNODES=320
+NODE_RANK=0
+export WORLD_SIZE=$(($CPUS_PER_NODE*$NNODES))
+export MASTER_ADDR=localhost
+export MASTER_PORT=$((10000 + ($PJM_JOBID % 50000)))
+CHECKPOINT_PATH=checkpoints/1_3b_mp4_dp16_v1_ja${JA_VOCAB_SIZE}K_en${EN_VOCAB_SIZE}K
+INPUT_PREFIX=dataset
+VOCAB_FILE=tokenizer/models/cc100ja1GB_cc100en1GB/cc100_ja${JA_VOCAB_SIZE}K_en${EN_VOCAB_SIZE}K.symbolRemoved.vocab.reestimated.model
+DATA_PATH=data/wikipedia/binarized/v1-ja${JA_VOCAB_SIZE}K-en${EN_VOCAB_SIZE}K/ja_wiki_text_document
+TENSORBOARD_ARGS="--tensorboard-dir experiments/tensorboard"
+
+output_path="jobs/mpi_outs/${PJM_JOBID}_n${nodos}"
+DISTRIBUTED_ARGS="-np $NNODES -std-proc ${output_path}/stdproc"
+
+DATA_PARALLEL_SIZE=16
+PIPELINE_MODEL_PARALLEL_SIZE=1
+TENSOR_MODEL_PARALLEL_SIZE=20
+
+PIPELINE_PARALLEL_ARGS="--pipeline-model-parallel-size $PIPELINE_MODEL_PARALLEL_SIZE"
+MODEL_PARALLEL_ARGS="--tensor-model-parallel-size $TENSOR_MODEL_PARALLEL_SIZE"
+
+DATA_PARALLEL_ARGS="--DDP-impl local"
+PARALLEL_ARGS="$MODEL_PARALLEL_ARGS $DATA_PARALLEL_ARGS $PIPELINE_PARALLEL_ARGS"
+
+export OMP_NUM_THREADS=48
+
+mpirun $DISTRIBUTED_ARGS \
+  python pretrain_gpt.py \
+    --num-layers 40 \
+    --hidden-size 5120 \
+    --num-attention-heads 40 \
+    --micro-batch-size 1 \
+    --global-batch-size 16 \
+    --seq-length 1024 \
+    --max-position-embeddings 1024 \
+    --train-iters 100 \
+    --lr-decay-iters 320000 \
+    --save $CHECKPOINT_PATH \
+    --load $CHECKPOINT_PATH \
+    --data-path $DATA_PATH \
+    --tokenizer-type JapaneseSentencePiece \
+    --vocab-file $VOCAB_FILE \
+    --data-impl mmap \
+    --split 949,50,1 \
+    --distributed-backend mpi \
+    --lr 0.00015 \
+    --min-lr 1.0e-5 \
+    --lr-decay-style cosine \
+    --weight-decay 1e-2 \
+    --clip-grad 1.0 \
+    --lr-warmup-fraction .01 \
+    --log-interval 1 \
+    --save-interval 10000 \
+    --eval-interval 1000 \
+    --eval-iters 10 \
+    --no-cuda \
+    --checkpoint-activations \
+    --use-cpu-initialization \
+    --num-workers 0 \
+    --no-load-rng \
+    $PARALLEL_ARGS \
+    $TENSORBOARD_ARGS \
+    --log-batch-size-to-tensorboard \
+    --log-validation-ppl-to-tensorboard \
+    --log-timers-to-tensorboard \
+    --wandb-name "ja-wiki-13b-dp16-tp20-ja${JA_VOCAB_SIZE}K-en${EN_VOCAB_SIZE}K"
