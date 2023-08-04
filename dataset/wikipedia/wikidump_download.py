9041c587c5d5f310b3e8ff8868e946a0beeeb7ba
kazuki <kaz.tokyo.tech20@gmail.com>
2023-08-04 21:19:59 +0900

    style: add type annotation

--- dataset/wikipedia/wikidump_download.py
+++ dataset/wikipedia/wikidump_download.py
@@ -1,10 +1,9 @@
 import os
-from IPython import embed
 from urllib.request import urlretrieve
 
 
 class Config(object):
-    def __init__(self, lang="ja"):
+    def __init__(self, lang="ja") -> None:
         self.corpus_name = "{}_wiki".format(lang)
 
         self.download_link = "https://dumps.wikimedia.org/{}wiki/latest/{}wiki-latest-pages-articles.xml.bz2".format(  # noqa
@@ -21,14 +20,14 @@ class Config(object):
         self.chunk_size = "100M"
 
 
-def download_data(config):
+def download_data(config: Config) -> None:
     if not os.path.exists(config.raw_data_path):
         print(f"Downloading {config.download_link} to {config.raw_data_path}")
         urlretrieve(config.download_link, config.raw_data_path)
         print(f"Successfully downloaded {config.raw_data_path}")
 
 
-def preproces_data(config):
+def preproces_data(config: Config) -> None:
     input_file = config.raw_data_path
     output_dir = config.processed_data_dir
     chunk_size = config.chunk_size
