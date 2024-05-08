74753f95e34cdde9321bd9788c4bacdc1ed00fdb
Kentaro Kawakami <kawakami.k@fujitsu.como>
2024-05-08 16:50:30 +0900

    Update copyright

--- megatron/data/dataset_utils.py
+++ megatron/data/dataset_utils.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 # Copyright 2018 The Google AI Language Team Authors, and NVIDIA.
+# Copyright (c) 2023, Fujitsu Limited.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
